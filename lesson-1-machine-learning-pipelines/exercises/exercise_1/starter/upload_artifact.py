83aa348f89b6b9fe109b69feb07c7a52c99f77eb
Mikel Sagardia <mxagar@gmail.com>
2022-07-07 13:53:08 +0200

    lesson 1, exercise 1: logginng and using artifacts

--- lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/upload_artifact.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/upload_artifact.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python
+'''This file creates and registers a Weights & Biases run using arguments passed from the CLI.
+
+To run it:
+
+python upload_artifact.py --input_file zen.txt \
+           --artifact_name zen_of_python \
+           --artifact_type text_file \
+           --artifact_description "20 aphorisms about writing good python code"
+           
+Then, check it on the W&B web interface: https://wandb.ai/home Projects: exercise_1
+
+If we change zen.txt and re-run the script, a new artifact version will appear in W&B.
+But if we re-run the script without changing the file no new version will appear.
+
+When we have different versions, we'll get a local folder artifacts/ which contains them. Whenever we'd like to use a version, the run object gets the correct path (see script use_artifact.py).
+'''
 import argparse
 import logging
 import pathlib
@@ -14,22 +29,24 @@ def go(args):
     logger.info("Creating run exercise_1")
 
     # Create a W&B run in the project ``exercise_1``. Set the option ``job_type="upload_file"``:
-
-    # YOUR CODE HERE
+    run = wandb.init(project="exercise_1",
+                     job_type="upload_file")
 
     # Create an instance of the class ``wandb.Artifact``. Use the ``artifact_name`` parameter to fill
     # the keyword ``name`` when constructing the wandb.Artifact class.
     # Use the parameters ``artifact_type`` and ``artifact_desc`` to fill respectively the keyword
     # ``type`` and ``description``
     # HINT: you can use args.artifact_name to reference the parameter artifact_name
-
-    # YOUR CODE HERE
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type, 
+        description=args.artifact_description,
+    )
 
     # Attach the file provided as the parameter ``input_file`` to the artifact instance using
     # ``artifact.add_file``, and log the artifact to the run using ``run.log_artifact``.
-
-    # YOUR CODE HERE
-
+    artifact.add_file(args.input_file)
+    run.log_artifact(artifact)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -58,3 +75,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     go(args)
+
+    
--- lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/use_artifact.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python
+'''This script uses an artifact registered in Weights & Biases using arguments passed from the CLI.
+
+To run it:
+
+python use_artifact.py --artifact_name exercise_1/zen_of_python:v1
+
+We can change between artifact versions with v0, v1, etc.
+
+The different artifact versions are on the cloud and also locally, in the folder artifacts/
+'''
 import argparse
 import logging
 import pathlib
@@ -16,13 +25,19 @@ def go(args):
 
     logger.info("Getting artifact")
 
-    # YOUR CODE HERE: get the artifact and store its local path in the variable "artifact_path"
-    # HINT: you can get the artifact path by using the "file()" method
-
-    artifact_path = artifact.file()
-
+    # Get the artifact and store its local path in the variable "artifact_path"
+    # From the W&B web interface (Usage):
+    # artifact = run.use_artifact('datamix-ai/exercise_1/zen_of_python:v1', type='text_file')
+    # Apparently, we don't necessarily need the team name datamix-ai
+    artifact = run.use_artifact(args.artifact_name)
+    # The file path is retreived, pointing to the correct version in folder artifacts/
+    artifact_filepath = artifact.file()
+    print(f"artifact_filepath = {artifact_filepath}")
+    
+    # We open the file
+    # But 
     logger.info("Artifact content:")
-    with open(artifact_path, "r") as fp:
+    with open(artifact_filepath, "r") as fp:
         content = fp.read()
 
     print(content)
