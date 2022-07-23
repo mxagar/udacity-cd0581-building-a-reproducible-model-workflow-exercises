d358d851c59d02e0a1ccab18a9df9a4befc301e5
Mikel Sagardia <mxagar@gmail.com>
2022-07-23 13:48:41 +0200

    exercise 5 completed

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -4,18 +4,45 @@ import logging
 import pandas as pd
 import wandb
 
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
-
 
 def go(args):
 
     run = wandb.init(project="exercise_5", job_type="process_data")
 
-    ## YOUR CODE HERE
-    pass
+    # Open the artifact: the name is not the filename,
+    # but the name we used when registering it
+    # To download the file we need to call .file()
+    logger.info("Downloading artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    df = pd.read_parquet(artifact.file())
+    
+    logger.info("Performing pre-processing: duplicates + new data")
+    # Drop duplicates
+    df = df.drop_duplicates().reset_index(drop=True)
+    # New feature
+    # This feature will have to go to the feature store.
+    # If you do not have a feature store,
+    # then you should not compute it here as part of the preprocessing step.
+    # Instead, you should compute it within the inference pipeline.
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
 
+    # Persist cleaned dataset
+    df.to_csv("preprocessed_data.csv", sep=',', header=True, index=False)
+
+    logger.info("Creating artifact")
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description,
+    )
+    artifact.add_file("preprocessed_data.csv")
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
