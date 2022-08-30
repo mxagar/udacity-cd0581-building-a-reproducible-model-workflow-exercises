935b493e6a87871bfa7f93ea84c1d04fed46a29f
Mikel <mxagar@gmail.com>
2022-08-30 13:11:27 +0200

    non-deterministic test

--- lesson-3-data-validation/exercises/exercise_8/starter/test_data.py
+++ lesson-3-data-validation/exercises/exercise_8/starter/test_data.py
@@ -10,6 +10,11 @@ run = wandb.init(project="exercise_8", job_type="data_tests")
 
 @pytest.fixture(scope="session")
 def data():
+    """Pytest fixture which provides the dataset in two separate dataframes (train and test splits).
+
+    Returns:
+        tuple of two dataframes: train and test splits of the dataset
+    """
 
     local_path = run.use_artifact("exercise_6/data_train.csv:latest").file()
     sample1 = pd.read_csv(local_path)
@@ -21,7 +26,13 @@ def data():
 
 
 def test_kolmogorov_smirnov(data):
+    """The statistical test Kolgomorov-Smirnov
+    is roughly the non-parametric equivalent of the T-test.
 
+    Args:
+        data (tuple of 2 dataframes): test and train splits automatically loaded in data() fixture.
+    """
+    
     sample1, sample2 = data
 
     numerical_columns = [
@@ -39,16 +50,23 @@ def test_kolmogorov_smirnov(data):
 
     # Let's decide the Type I error probability (related to the False Positive Rate)
     alpha = 0.05
-    # Bonferroni correction for multiple hypothesis testing
-    # (see my blog post on this topic to see where this comes from:
+    # Bonferroni correction for multiple hypothesis testing.
+    # We are comparing 2 splits consisting of several columns; thus,
+    # several tests are carried out and we need Bonferroni.
+    # See blog post on this topic:
     # https://towardsdatascience.com/precision-and-recall-trade-off-and-multiple-hypothesis-testing-family-wise-error-rate-vs-false-71a85057ca2b)
     alpha_prime = 1 - (1 - alpha)**(1 / len(numerical_columns))
 
     for col in numerical_columns:
 
-        # Use the 2-sample KS test (scipy.stats.ks_2sample) on the column
-        # col
-        ts, p_value = # YOUR CODE HERE
+        ts, p_value = scipy.stats.ks_2samp(
+            sample1[col],
+            sample2[col],
+            alternative='two-sided'
+        )
 
-        # Add an assertion so that the test fails if p_value > alpha_prime
-        # YOUR CODE HERE
+        # NOTE: as always, the p-value should be interpreted as the probability of
+        # obtaining a test statistic (TS) equal or more extreme that the one we got
+        # by chance, when the null hypothesis is true. If this probability is not
+        # large enough, this dataset should be looked at carefully, hence we fail
+        assert p_value > alpha_prime
